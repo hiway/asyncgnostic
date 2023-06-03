24742b64df23556d8963b44b186d5e9cc8fcf39b
Harshad Sharma <harshad@sharma.io>
2023-06-03 09:57:39 +0530

    Move example to examples/

--- examples/readme_example.py
+++ examples/readme_example.py
@@ -2,22 +2,27 @@ import asyncio
 from asyncgnostic import awaitable
 
 
+# Define a sync function
 def handler() -> str:  # type: ignore
     return "Running Sync"
 
 
+# Pair the sync function with an async function
 @awaitable(handler)
 async def handler() -> str:
     return "Running Async"
 
 
+# Call the function from sync context
 def sync_main():
     print("sync context", handler())
 
 
+# Call the function from async context
 async def async_main():
     print("async context:", await handler())
 
 
+# Run the sync and async functions
 sync_main()
 asyncio.run(async_main())
