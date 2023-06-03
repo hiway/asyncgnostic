ee50ba84b5cb4a22432e956a2a83bee2297ba49e
Harshad Sharma <harshad@sharma.io>
2023-06-03 08:03:06 +0530

    Borrow improvements from curio.

--- example.py
+++ example.py
@@ -1,44 +1,23 @@
 import asyncio
-import timeit
-
-from asyncgnostic import is_async
+from asyncgnostic import awaitable
 
 
-async def async_handler():
-    return "Running async"
+def handler() -> str:  # type: ignore
+    return "Running Sync"
 
 
-def sync_handler():
-    return "Running sync"
-
-
-def agnostic_handler():
-    if is_async(stack_depth=2):
-        return async_handler()
-    else:
-        return sync_handler()
-
-
-async def async_caller():
-    return await agnostic_handler()
-
-
-def sync_caller():
-    return agnostic_handler()
-
-
-async def async_main():
-    print("Calling from async_main, async:", await async_caller())
-    print("Calling from async_main, sync:", sync_caller())
+@awaitable(handler)
+async def handler() -> str:
+    return "Running Async"
 
 
 def sync_main():
-    print("Calling from sync_main, async:", asyncio.run(async_caller()))
-    print("Calling from sync_main, sync:", sync_caller())
+    print("sync context", handler())
+
+
+async def async_main():
+    print("async context:", await handler())
 
 
 sync_main()
 asyncio.run(async_main())
-
-
-print(timeit.timeit(is_async, number=100))
--- src/asyncgnostic/__init__.py
+++ src/asyncgnostic/__init__.py
@@ -1,22 +1,97 @@
+from sys import _getframe
+from functools import wraps as _wraps
 import inspect as _inspect
-from typing import Optional as _Optional
 
 
-def is_async(stack_depth: _Optional[int]=None):
-    stack = _inspect.stack()[1:]
-    for i, frame in enumerate(stack):
-        func_name = frame.function
-        
-        if func_name == "<module>":
-            continue
+def awaited(depth=2, _cache={}) -> bool:
+    """
+    Check if function was called from async context.
 
-        if stack_depth and i == stack_depth:
-            break
+    Args:
+        depth (int): Stack depth to check. Default is 2.
 
-        for frame in _inspect.stack()[1:]:
-            if func_name in frame.frame.f_locals:
-                func = frame.frame.f_locals[func_name]
-                if _inspect.iscoroutinefunction(func):
-                    return True
+    Returns:
+        bool: True if function was called from async context, False otherwise.
+    """
+    f_code = _getframe(depth).f_code
+    if f_code in _cache:
+        # Return cached value
+        return _cache[f_code]
 
-    return False
+    if f_code.co_flags & (
+        _inspect.CO_COROUTINE
+        | _inspect.CO_ITERABLE_COROUTINE
+        | _inspect.CO_ASYNC_GENERATOR
+    ):
+        # Context is async
+        _cache[f_code] = True
+        return True
+    else:
+        # Context is possibly sync, except if the function is called
+        # from a list comprehension or a generator expression.
+        # The name of the function will start with '<', such as <listcomp> or <genexpr>.
+        if f_code.co_flags & _inspect.CO_NESTED and f_code.co_name[0] == "<":
+            return awaited(depth + 2)
+        else:
+            _cache[f_code] = False
+            return False
+
+
+def awaitable(sync_func):
+    """
+    Decorator to pair a sync function with an async function.
+
+    Args:
+        sync_func (function): Sync function to pair with an async function.
+
+    Returns:
+        function: Decorated function.
+
+    Example:
+        >>> from asyncgnostic import awaitable
+
+        >>> def increment(x: int) -> int:
+        ...     return x + 1
+
+        >>> @awaitable(increment)
+        ... async def increment(x: int) -> int:
+        ...     return x + 1
+
+        >>> increment(1)
+        2
+
+        >>> await increment(1)
+        2
+    """
+
+    def decorate(async_func):
+        # Ensure sync and async functions have the same return type
+        if (
+            _inspect.signature(sync_func).return_annotation
+            != _inspect.signature(async_func).return_annotation
+        ):
+            raise TypeError(
+                f"{sync_func.__name__} and async {async_func.__name__} must have the same return type"
+            )
+
+        # Ensure sync and async functions have the same signature
+        if _inspect.signature(sync_func) != _inspect.signature(async_func):
+            raise TypeError(
+                f"{sync_func.__name__} and async {async_func.__name__} must have the same signature"
+            )
+
+        @_wraps(async_func)
+        def wrapper(*args, **kwargs):
+            if awaited():
+                # If called from async context, call async function
+                return async_func(*args, **kwargs)
+            else:
+                # If called from sync context, call sync function
+                return sync_func(*args, **kwargs)
+
+        # Set docstring
+        wrapper.__doc__ = sync_func.__doc__ or async_func.__doc__
+
+        return wrapper
+
+    return decorate
